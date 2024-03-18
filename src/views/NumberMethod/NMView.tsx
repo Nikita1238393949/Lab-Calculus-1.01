ce2e67511d5ff52bb343b252a269260cf56f6815
Nikita <146779463+Nikita1238393949@users.noreply.github.com>
2024-03-18 18:45:15 +0300

    Обновление пакетов

--- src/views/NumberMethod/NMView.tsx
+++ src/views/NumberMethod/NMView.tsx
@@ -1,9 +1,8 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import DarbouxSumsChart from "../../components/NumberMethod/DarbouxSumCharts";
-import {Button, Card, Col, Collapse, Form, Input, Radio, Row, Typography} from "antd";
+import {Button, Card, Col, Form, Input, Radio, Row, Typography} from "antd";
 import RiemannSumsChart from "../../components/NumberMethod/RiemannSumsChart";
 import {CheckOutlined} from "@ant-design/icons";
-
 
 interface RiemannValues {
     Nvalue: number
@@ -12,8 +11,15 @@ interface RiemannValues {
 
 const NMView = () => {
     const [darbouxDataState, setdarbouxDataState] = useState(5)
-    const [riemannDataState, setRiemannDataState] = useState<RiemannValues>({Nvalue: 5, TypeOf: "random"});
+    const [riemannDataState, setRiemannDataState] = useState<RiemannValues>({Nvalue: 5, TypeOf: "left"});
     const [RiemanForm] = Form.useForm()
+    const [DarbouxForm] = Form.useForm()
+    useEffect(() => {
+        RiemanForm.setFieldsValue({
+            NValue: 5,
+            TypeOf: "left"
+        });
+    }, []);
     return (
         <Row gutter={16}>
             <Col span={11}>
@@ -21,45 +27,49 @@ const NMView = () => {
                 <Card>
                     <DarbouxSumsChart n={darbouxDataState}/>
                 </Card>
-                <Collapse style={{marginTop: "30px"}} items={[{
-                    key: '1', label: "Try It Out", children:
-                        <>
-                            <Typography.Title style={{marginTop: "10px"}} level={4}>Текущее n
-                                = {darbouxDataState}</Typography.Title>
-                            <Input onChange={(e) => setdarbouxDataState(Number(e.target.value))}
-                                   placeholder={"Введите N"} defaultValue={5}/>
-                        </>
-                }]}/>
+                <Card style={{marginTop: "15px"}}>
+                    <Form form={DarbouxForm} onFinish={(values) => setdarbouxDataState(values.DarbouxN)}>
+                        <Form.Item name={"DarbouxN"} label={"Количество разбиений"}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button icon={<CheckOutlined/>} type={"primary"} htmlType={"submit"}>
+                                Построить
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
             </Col>
             <Col span={11} style={{marginLeft: "25px"}}>
                 <Typography.Title level={3}>Графики интегральных сумм</Typography.Title>
                 <Card>
                     <RiemannSumsChart n={riemannDataState.Nvalue} method={riemannDataState.TypeOf}/>
                 </Card>
-                <Collapse style={{marginTop: "30px"}} items={[{
-                    key: "2", label: "Try it Out", children:
-                        <Form form={RiemanForm} onFinish={(values) => setRiemannDataState({
+                <Card style={{marginTop: "15px"}}>
+                    <Form form={RiemanForm} onFinish={(values) => {
+                        setRiemannDataState({
                             Nvalue: values.NValue,
                             TypeOf: values.TypeOf
-                        })}>
-                            <Form.Item name={"NValue"} label={"Разбиения"}>
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item name={"TypeOf"} label={"Тип оснащения"}>
-                                <Radio.Group>
-                                    <Radio value={"left"}>Левые</Radio>
-                                    <Radio value={"right"}>Правые</Radio>
-                                    <Radio value={"middle"}>Средние</Radio>
-                                    <Radio value={"random"}>Случайные</Radio>
-                                </Radio.Group>
-                            </Form.Item>
-                            <Form.Item>
-                                <Button icon={<CheckOutlined/>} type={"primary"} htmlType={"submit"}>
-                                    Построить
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                }]}/>
+                        });
+                    }}>
+                        <Form.Item name={"NValue"} label={"Разбиения"}>
+                            <Input defaultValue={5}/>
+                        </Form.Item>
+                        <Form.Item name={"TypeOf"} label={"Тип оснащения"}>
+                            <Radio.Group defaultValue={"left"}>
+                                <Radio value={"left"}>Левые</Radio>
+                                <Radio value={"right"}>Правые</Radio>
+                                <Radio value={"middle"}>Средние</Radio>
+                                <Radio value={"random"}>Случайные</Radio>
+                            </Radio.Group>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button icon={<CheckOutlined/>} type={"primary"} htmlType={"submit"}>
+                                Построить
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
             </Col>
         </Row>
     )
