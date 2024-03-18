351ff7fd82ed6bf997886c9c7d2e512a3573bb87
Nikita <146779463+Nikita1238393949@users.noreply.github.com>
2024-03-18 12:39:10 +0300

    котики + реформат

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {ConfigProvider, theme} from "antd";
-const BaseViewPage = React.lazy(()=>
+
+const BaseViewPage = React.lazy(() =>
     import("./views/BaseView")
 )
+
 function App() {
-  const router = createBrowserRouter([
-    {path: "/",element:<BaseViewPage/>}
-  ])
-  document.body.style.backgroundColor = "black"
-  return (
-      <ConfigProvider theme={{algorithm:theme.darkAlgorithm}}>
-    <RouterProvider router={router}/>
-      </ConfigProvider>
-  );
+    const router = createBrowserRouter([
+        {path: "/", element: <BaseViewPage/>}
+    ])
+    document.body.style.backgroundColor = "black"
+    return (
+        <ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
+            <RouterProvider router={router}/>
+        </ConfigProvider>
+    );
 }
 
 export default App;
--- src/views/BaseView.tsx
+++ src/views/BaseView.tsx
@@ -1,54 +1,60 @@
-import React, {useState} from "react";
-import {Button, Card, Col, Layout, Modal, Row, Typography} from "antd";
+import React from "react";
+import {Button, Card, Col, Layout, Row, Typography} from "antd";
 import 'katex/dist/katex.min.css';
-import { InlineMath} from 'react-katex';
-import {EditOutlined, FilePdfOutlined} from "@ant-design/icons";
-const BaseView = () =>{
-    const [PDFOpen , setPDFOpen] = useState(false)
-    return(
-        <Layout style={{margin:"50px"}}>
+import {InlineMath} from 'react-katex';
+import {EditOutlined} from "@ant-design/icons";
+
+const BaseView = () => {
+    return (
+        <Layout style={{margin: "50px"}}>
             <Row gutter={16}>
                 <Col span={12}>
-                   <Card>
-                       <Typography.Title>Лабораторная работа 1.01</Typography.Title>
-                       <Typography.Title level={3}>Аналитическая часть</Typography.Title>
-                       <ol>
-                           <Typography.Text>
-                           <li>
-                               Построить верхнюю и нижнюю суммы Дарбу для равномерного разбиения на n частей.
-                           </li>
-                           <li>
-                               Доказать, что функция интегрируема по Риману.
-                           </li>
-                           <li>
-                               Найти пределы сумм Дарбу, найти значения интеграла.
-                           </li>
-                           <li>
-                               Сравнить со значением, полученным по формуле Ньютона-Лейбница.
-                           </li>
-                           </Typography.Text>
-                       </ol>
-                   </Card>
-                    <Card style={{marginTop:"17px"}}>
+                    <Card>
+                        <Typography.Title>Лабораторная работа 1.01</Typography.Title>
+                        <Typography.Title level={3}>Аналитическая часть</Typography.Title>
+                        <ol>
+                            <Typography.Text>
+                                <li>
+                                    Построить верхнюю и нижнюю суммы Дарбу для равномерного разбиения на n частей.
+                                </li>
+                                <li>
+                                    Доказать, что функция интегрируема по Риману.
+                                </li>
+                                <li>
+                                    Найти пределы сумм Дарбу, найти значения интеграла.
+                                </li>
+                                <li>
+                                    Сравнить со значением, полученным по формуле Ньютона-Лейбница.
+                                </li>
+                            </Typography.Text>
+                        </ol>
+                    </Card>
+                    <Card style={{marginTop: "17px"}}>
                         <Typography.Title level={3}>Численный метод</Typography.Title>
                         <ol>
                             <li>
-                                Построить графики <InlineMath math={"f(x)"}/> и ступенчатые фигуры,соответсвующие суммам Дарбу для некоторых значений <InlineMath math={'n'}/>. Добавить их значения в таблицу в отчете. (см. Замечания)
+                                Построить графики <InlineMath math={"f(x)"}/> и ступенчатые фигуры,соответсвующие суммам
+                                Дарбу для некоторых значений <InlineMath math={'n'}/>. Добавить их значения в таблицу в
+                                отчете. (см. Замечания)
                             </li>
                             <li>
-                                Построить графики интегральных сумм со случайными оснащениями <InlineMath math={"\\xi"}/> для тех же разбиений. Добавить их зачения в таблицу в отчете.
+                                Построить графики интегральных сумм со случайными оснащениями <InlineMath
+                                math={"\\xi"}/> для тех же разбиений. Добавить их зачения в таблицу в отчете.
                             </li>
                             <li>
-                                По желанию написать программу,вычисляющую приближённое значение интеграла для равномерного разбиения методом трапеций. Нарисовать рисунок, сравнить результаты.
+                                По желанию написать программу,вычисляющую приближённое значение интеграла для
+                                равномерного разбиения методом трапеций. Нарисовать рисунок, сравнить результаты.
                             </li>
                         </ol>
                         <Typography.Title level={4}>Замечания</Typography.Title>
                         <ol>
                             <li>
-                                Входные данные программы: число точек,разбиения,способ выбора оснащения (левые, правые, средние, случайные).
+                                Входные данные программы: число точек,разбиения,способ выбора оснащения (левые, правые,
+                                средние, случайные).
                             </li>
                             <li>
-                                В таблицу добавить результаты минимум для 3 разных n. По желанию можно графически показать для большего количества различных n.
+                                В таблицу добавить результаты минимум для 3 разных n. По желанию можно графически
+                                показать для большего количества различных n.
                             </li>
                         </ol>
                     </Card>
@@ -74,21 +80,26 @@ const BaseView = () =>{
                             </li>
                         </ol>
                     </Card>
-                    <Card style={{marginTop:"17px"}}>
+                    <Card style={{marginTop: "17px"}}>
                         <Typography.Title level={3}> Вариант №1</Typography.Title>
-                        <Typography style={{fontSize:"25px", marginTop:"25px" ,marginBottom:"25px"}}>
-                        <InlineMath math={"f(x)=x^2 \\quad [1,2]"}/>
+                        <Typography style={{fontSize: "25px", marginTop: "25px", marginBottom: "25px"}}>
+                            <InlineMath math={"f(x)=x^2 \\quad [1,2]"}/>
                         </Typography>
-                        <Button icon={<EditOutlined />} type={"primary"} style={{minWidth:"250px"}}>
+                        <Button icon={<EditOutlined/>} type={"primary"} style={{minWidth: "250px"}}>
                             Перейти к решению
                         </Button>
                     </Card>
-                    <Card style={{marginTop:"17px"}}>
-                        <Typography.Title level={3}>Условия работы</Typography.Title>
-                       <Button icon={<FilePdfOutlined />} type={"primary"} style={{height:"75px",marginTop:"40px"}} onClick={()=>setPDFOpen(true)}> Открыть документ</Button>
-                        <Modal open={PDFOpen} footer={[]} onCancel={()=>setPDFOpen(false)}>
-                            <iframe src={"https://drive.google.com/uc?export=download&id=1BoK2Qyho4XFzzsfnVSILjQwCr7maa5Sg"}></iframe>
-                        </Modal>
+                    <Card style={{marginTop: "17px"}}>
+                         <pre style={{margin: "4px"}}>
+      {`
+                                      ___
+             |\\__/,|   (\`\\        _.-|   |          |\\__/,|   (\`\\
+             |o o  |__ _) )      {   |   |          |o o  |__ _) )
+           _.( T   )  \`  /        "-.|___|        _.( T   )  \`  /
+ n n._    ((_ \`^--' /_<  \\         .--'-\`-.     _((_ \`^--' /_<  \\
+ <" _ }=- \`\` \`-'(((/  (((/       .+|______|__.-||__)\`-'(((/  (((/
+      `}
+    </pre>
                     </Card>
                 </Col>
             </Row>
